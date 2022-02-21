4c2fc3b7e4a7616395a64752b804cc21245dfa45
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2022-02-21 20:39:22 +0300

    Spring. Training Spring 2 lesson 3

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO students(id, fio, age) VALUES(1, 'Ivanov', 20);
+INSERT INTO students(id, fio, age) VALUES(2, 'Petrov', 21);
+INSERT INTO students(id, fio, age) VALUES(3, 'Ivan', 22);
+INSERT INTO students(id, fio, age) VALUES(4, 'Kim', 25);
+
+INSERT INTO animal(id, vie, wei) VALUES(1, 'Giraffe', 650);
+INSERT INTO animal(id, vie, wei) VALUES(2, 'Monkey', 55);
+INSERT INTO animal(id, vie, wei) VALUES(3, 'Trout', 12);
+INSERT INTO animal(id, vie, wei) VALUES(4, 'Human', 82)
