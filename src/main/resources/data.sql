1f84e1b5e14fdbbbf6fef123eeeb961b9acb0abb
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2022-03-03 21:13:01 +0300

    Spring. Training Spring 2 lesson 7

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,3 @@
-INSERT INTO faculties(id, name)
-VALUES(1, 'Java Development');
-INSERT INTO faculties(id, name)
-VALUES(2, 'Java Automation');
-
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(1, 'Ivanov', 20, 1);
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(2, 'Petrov', 30, 1);
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(3, 'Ivan', 22, 1);
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(4, 'Kim', 25, 2);
 
 INSERT INTO pets(id, homePet)
 VALUES(1, 'Homemade');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,5 @@
-DROP TABLE IF EXISTS students, faculties;
+
 DROP TABLE IF EXISTS animals, pets;
-
-CREATE TABLE students(
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-fio VARCHAR(256),
-age INT,
-faculties_id BIGINT
-);
-
-CREATE TABLE faculties(
-id BIGINT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(256)
-);
-
-ALTER TABLE students add foreign key (faculties_id) references faculties(id);
 
 CREATE TABLE animals(
 id BIGINT PRIMARY KEY AUTO_INCREMENT,
