08291f8f7d7930a5b210a2dbca2a41caa4026b12
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2022-03-03 21:09:39 +0300

    Spring. Training Spring 2 lesson 7

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,10 +1,3 @@
-INSERT INTO faculties(id, name)
-VALUES(1, 'Android Development');
-
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(1, 'Ivanov', 20, 1);
-INSERT INTO students(id, fio, age, faculties_id)
-VALUES(2, 'Petrov', 30, 1);
 
 INSERT INTO pets(id, homePet)
 VALUES(1, 'Dog');
